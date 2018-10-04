06229fe42cf690fd76ba98e44be491b9b446cee0
DavidMkrtchyan7 <davidmkrtchyan7@gmail.com>
2018-10-04 14:31:41 +0500

    palindromes

---
+++ palindromes.py
@@ -0,0 +1,12 @@
+﻿def palindrome(text: str):
+    l = len(text)
+    for i in range(l//2):
+        if text[i] != text[l-i-1]:
+            return False
+    return True
+while True:
+    text = input("Enter the text:")
+    if text == '':
+        print("Hello")
+        break
+    print("The text", text, "is a palindrome" if palindrome(text) else "is not a palindrome")
