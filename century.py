4e20b36f7ec110ee233ba68a8af137cc9ea177c1
DavidMkrtchyan7 <davidmkrtchyan7@gmail.com>
2018-10-04 13:54:08 +0500

    century

---
+++ century.py
@@ -0,0 +1,8 @@
+﻿def century(n):
+    return (n+99)//100
+while True:
+    year = int(input("Enter the year  "))
+    if not year:
+        print("False")
+        break
+    print("Year: %s  Century: %s" % (year, century(year)))
